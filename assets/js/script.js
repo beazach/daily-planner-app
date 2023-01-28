fa30a66ab858bb76e168e1d352a4b20426cbcf52
bea zacharska <beata.zacharska@gmail.com>
2023-01-28 16:11:08 +0000

    saved the event in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
     //Storing the value of each click
     let timeID = $(this).parent().attr("id")  //Storing save button click and hour (parent)
     let value = $(this).siblings(".description").val(); //Storing the text values (siblings)
-    
     console.log(timeID);
     console.log(value)
-
+    localStorage.setItem(timeID, value); //Setting up the local storage
+    $(".notification").addClass('show');
     })
 
 

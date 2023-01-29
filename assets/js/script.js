4fe7b0f9bc6cf9e5f8ebbc3fb224e7a8441348f1
bea zacharska <beata.zacharska@gmail.com>
2023-01-29 17:32:22 +0000

    added a function to colour code past, present and future timeblocks and text center notification about local storage updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,29 @@ $(document).ready(function(){
     $(".notification").addClass('show');
     })
 
+// Adding a function to colour code past, present and future timeblocks     
+function timer(){
+    let currentHour = moment().hours();
+    for(let i = 0; i < $(".time-block").length; i++){
+        let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1])
+        console.log(hour)
+        console.log(currentHour)
+        if(hour<currentHour){
+            $(".time-block")[i].classList.add("past")
+        } else if(hour === currentHour){
+            $(".time-block")[i].classList.add("present")
+            $(".time-block")[i].classList.remove("past")
+        } else {
+            $(".time-block")[i].classList.remove("past")
+            $(".time-block")[i].classList.remove("present")
+            $(".time-block")[i].classList.add("future")
+        }
+    }
+}
+timer();
 
+let interval = setInterval(timer, 50000);
+
+// Setting uo the current date
+$("#currentDay").text(moment().format("dddd, MMMM, Do"))
 })
